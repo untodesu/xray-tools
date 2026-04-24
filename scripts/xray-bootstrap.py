1e09bfc82e8a3d1540aab1ceb654828f522af797
untodesu <untodesu@proton.me>
2026-04-24 23:15:27 +0500

    Select 30% of random outbounds

--- scripts/xray-bootstrap.py
+++ scripts/xray-bootstrap.py
@@ -1010,7 +1010,9 @@ def xrb_auto_setup(screen, xray_config):
     progress = UU_ProgressBar(screen, "Running auto-setup...", total)
     created = 0
 
-    for sni in vless_sni_predefs:
+    vless_sni_predefs_r = random.sample(vless_sni_predefs, k=round(0.3 * len(vless_sni_predefs)))
+
+    for sni in vless_sni_predefs_r:
         ports = [443]
         if add_random_ports:
             ports.append(random.randrange(1024, 5120))
